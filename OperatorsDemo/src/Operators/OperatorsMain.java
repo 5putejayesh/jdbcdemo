246f9a1575163caf8e3173311a311c82bd439a90
PachputeJayesh <PachputeJayesh@LAPTOP-2RI2BBN0>
2021-08-25 12:44:51 +0530

    changed msg fo user input.

--- OperatorsDemo/src/Operators/OperatorsMain.java
+++ OperatorsDemo/src/Operators/OperatorsMain.java
@@ -15,6 +15,7 @@ public class OperatorsMain {
 		
 		int arr[]=new int[3];
 		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter Array Elements:");
 		for (int i=0;i<arr.length;i++){
 			arr[i]=sc.nextInt();
 		}
